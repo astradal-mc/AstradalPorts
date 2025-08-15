795d6fd3be5b716ed7ef0ab7e8fbbdfd7e3154b4
Pookachu <ethandanielhunt@gmail.com>
2025-08-15 02:58:12 -0500

    Prevent the onPlayerInteract event from being caught twice.

--- src/main/java/net/astradal/astradalPorts/listeners/PortstoneInteractionListener.java
+++ src/main/java/net/astradal/astradalPorts/listeners/PortstoneInteractionListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class PortstoneInteractionListener implements Listener {
     private final PortstoneManager portstoneManager;
@@ -21,6 +22,10 @@ public class PortstoneInteractionListener implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getHand() != EquipmentSlot.HAND) {
+            return;
+        }
+
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
 
         Block clickedBlock = event.getClickedBlock();
